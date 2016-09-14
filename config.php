d6d30c5c9289ddc0948abcb5aa2e87198d42161a
ybzhang <286267616@qq.com>
2016-09-14 09:17:10 +0800

    配置文件修改

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 if (!defined('IN_CONTEXT')) die('access violation error!');
 class Config {
 public static $mysql_ext = 'mysql';
-public static $db_host = '';
-public static $db_user = 'ssyear';
-public static $db_pass = 'zrswnaww';
-public static $db_name = 'ssyear';
+public static $db_host = 'localhost';
+public static $db_user = 'root';
+public static $db_pass = '';
+public static $db_name = 'sitestar';
 public static $port = '3306';
 public static $mysqli_charset = 'utf8';
 public static $tbl_prefix = 'ss_';
