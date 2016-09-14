ed33d4c7ecae03ac3913bdcb4af3c30190f4fb38
ybzhang <286267616@qq.com>
2016-09-14 09:04:43 +0800

    初始版本

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+if (!defined('IN_CONTEXT')) die('access violation error!');
+class Config {
+public static $mysql_ext = 'mysql';
+public static $db_host = '';
+public static $db_user = 'ssyear';
+public static $db_pass = 'zrswnaww';
+public static $db_name = 'ssyear';
+public static $port = '3306';
+public static $mysqli_charset = 'utf8';
+public static $tbl_prefix = 'ss_';
+public static $cookie_prefix = 'YoBrKc_';
+public static $enable_db_debug = false;
+}?>
